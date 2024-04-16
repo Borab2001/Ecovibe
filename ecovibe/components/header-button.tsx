1d51697af95a641aedf2941fab4bf48ea2f2bb7b
Bora <borabalos9@gmail.com>
2024-04-16 11:23:45 +0200

    Active state for button

--- ecovibe/components/header-button.tsx
+++ ecovibe/components/header-button.tsx
@@ -1,15 +1,28 @@
-const HeaderButton = () => {
+interface HeaderButtonProps {
+    isActive: boolean;
+    setIsActive: (isActive: boolean) => void;
+}
+
+const HeaderButton: React.FC<HeaderButtonProps> = ({
+    isActive,
+    setIsActive
+}) => {
     return (
-        <div className="h-10 w-24 bg-green-500 rounded-3xl cursor-pointer relative overflow-hidden">
-            <div className="w-full h-full bg-green-400 text-black flex items-center justify-center uppercase">
-                <p>
-                    Menu
-                </p>
-            </div>
-            <div className="w-full h-full bg-black text-white flex items-center uppercase justify-center absolute top-full">
-                <p>
-                    Close
-                </p>
+        <div
+            onClick={() => {setIsActive(!isActive)}} 
+            className="h-10 w-24 bg-green-500 rounded-3xl cursor-pointer relative overflow-hidden"
+        >
+            <div className="">
+                <div className="w-full h-full bg-green-400 text-black flex items-center justify-center uppercase">
+                    <p>
+                        Menu
+                    </p>
+                </div>
+                <div className="w-full h-full bg-black text-white flex items-center uppercase justify-center absolute top-full">
+                    <p>
+                        Close
+                    </p>
+                </div>
             </div>
         </div>
     );
--- ecovibe/components/ui/header.tsx
+++ ecovibe/components/ui/header.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import HeaderButton from "@/components/header-button";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
     return (
         <header className="fixed right-4 top-4">
-            <HeaderButton />
+            <HeaderButton isActive={isActive} setIsActive={setIsActive} />
         </header>
     );
 }
