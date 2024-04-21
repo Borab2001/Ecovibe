4206c1f15de3dc7eac2667eda67b975f08dfc9f0
Bora <borabalos9@gmail.com>
2024-04-21 17:32:50 +0200

    Animated socials in navbar

--- ecovibe/components/ui/header-nav.tsx
+++ ecovibe/components/ui/header-nav.tsx
@@ -27,6 +27,25 @@ const Nav = () => {
         },
     ];
 
+    const socials = [
+        {
+            name: 'Facebook',
+            url: '/',
+        },
+        {
+            name: 'LinkedIn',
+            url: '/',
+        },
+        {
+            name: 'Instagram',
+            url: '/',
+        },
+        {
+            name: 'X',
+            url: '/',
+        },
+    ];
+
     const animated = {
         initial: {
             opacity: 0,
@@ -44,31 +63,66 @@ const Nav = () => {
             }
         }),
         exit: {
-            opacity: 0
+            opacity: 0,
+            // transition: { ease: [0.76, 0, 0.24, 1] }
+        }
+    }
+
+    const slideIn = {
+        initial: {
+            opacity: 0,
+            translateY: 20,
+        },
+        enter: (index: number) => ({
+            opacity: 1,
+            translateY: 0,
+            transition: {
+                duration: 0.5,
+                delay: 0.75 + (index * 0.1),
+                ease: [0.215, 0.61, 0.355, 1]
+            }
+        }),
+        exit: {
+            opacity: 0,
+            // transition: { ease: [0.76, 0, 0.24, 1] }
         }
     }
 
     return (
-        <nav className="h-full pt-24 px-10 pb-12 box-border">
+        <nav className="flex flex-col justify-between h-full pt-24 px-10 pb-12 box-border">
             <div className="flex flex-col gap-4">
                 {links.map((link, index) => (
-                    <div
+                    <motion.div 
                         key={index}
+                        className="text-4xl text-black"
+                        custom={index}
+                        variants={animated}
+                        animate="enter"
+                        exit="exit"
+                        initial="initial"
                     >
-                        <motion.div 
-                            
-                            className="text-4xl text-black hover:text-black"
-                            custom={index}
-                            variants={animated}
-                            animate="enter"
-                            exit="exit"
-                            initial="initial"
-                        >
-                            <Link href={link.url}>
-                                {link.name}
-                            </Link>
-                        </motion.div>
-                    </div>
+                        <Link href={link.url}>
+                            {link.name}
+                        </Link>
+                    </motion.div>
+                ))}
+            </div>
+
+            <div className="flex flex-wrap">
+                {socials.map((social, index) => (
+                    <motion.div
+                        key={`social_${index}`}
+                        className="w-1/2 text-black"
+                        custom={index}
+                        variants={slideIn}
+                        animate="enter"
+                        exit="exit"
+                        initial="initial"
+                    >
+                        <Link href={social.url}>
+                            {social.name}
+                        </Link>
+                    </motion.div>
                 ))}
             </div>
         </nav>
